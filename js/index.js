2187ff1adfae491d7bd761c8207a0c4c38621868
maxfreeborn <mfreeborn90@gmail.com>
2023-11-29 12:04:24 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,18 @@
-$(document).scroll(function() {
-  var $header = $('.header-content');
-  var $footer = $('footer'); // Replace with your footer selector if different
-  var footerPosition = $footer.offset().top;
-  
-  if ($(window).scrollTop() + $(window).height() < footerPosition) {
-    // The footer is not yet reached
-    $header.css({
-      'position': 'sticky',
-      'top': '0'
+$(document).ready(function() {
+    // Now the DOM is fully loaded, and we can safely attach our scroll event listener
+    $(document).scroll(function() {
+        var $header = $('.header-content');
+        var $footer = $('footer'); // Replace with your footer selector if different
+        var footerPosition = $footer.offset().top;
+
+        if ($(window).scrollTop() + $(window).height() < footerPosition) {
+            // The footer is not yet reached
+            $header.css({
+                'position': 'sticky',
+                'top': '0'
+            });
+        } else {
+            $header.css('position', 'absolute');
+        }
     });
-  } else {
-        $header.css('position', 'absolute');
-  }
 });
